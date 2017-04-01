3fdc3dcbec172eec83fbc4322178546c1f22f1cd
Firenzo <firenzo_akkefietje@msn.com>
2017-04-01 20:34:21 +0200

    optimizations

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -6,8 +6,6 @@ $(document).ready(function() {
 
 		app.controller = {
 			init: function(){
-				//app.checkWindow.resize();
-				//app.checkWindow.width();
 				app.scroll.init();
 				app.menu.init();
 
@@ -18,29 +16,6 @@ $(document).ready(function() {
 
 			menu:0
 
-		},
-
-		app.checkWindow = {
-			resize: function(){
-				window.onresize = function() {
-    				if (window.innerWidth <= 800 && document.getElementById("mobileGallery") === null){
-    					if (document.getElementById("desktopGallery") != null) {$("#desktopGallery").remove();}
-    					/*$("section#Foto div.row").append("<iframe id='mobileGallery' src='http://lightwidget.com/widgets/6f64a869633c5377a89738d158da0704.html' scrolling='no' allowtransparency='true' class='lightwidget-widget' style='width: 100%; border: 0; overflow: hidden;'></iframe>")*/
-    			
-    				} else if (window.innerWidth > 800 && document.getElementById("desktopGallery") === null){
-    					if (document.getElementById("mobileGallery") != null) {$("#mobileGallery").remove();}
-    					/*$("section#Foto").append("<iframe id ='desktopGallery' src='http://lightwidget.com/widgets/b5edcd0f9f2e57beb7aaacb816f1a708.html' scrolling='no' allowtransparency='true' class='lightwidget-widget' style='width: 100%; border: 0; overflow: hidden;'></iframe>");*/
-    				}
-				}
-			},
-
-			width: function(){
-				if (window.innerWidth <= 800) {
-					/*$("section#Foto div.row").append("<iframe id='mobileGallery' src='http://lightwidget.com/widgets/6f64a869633c5377a89738d158da0704.html' scrolling='no' allowtransparency='true' class='lightwidget-widget' style='width: 100%; border: 0; overflow: hidden;'></iframe>")*/
-				} else if (window.innerWidth > 800){
-					/*$("section#Foto").append("<iframe id ='desktopGallery' src='http://lightwidget.com/widgets/b5edcd0f9f2e57beb7aaacb816f1a708.html' scrolling='no' allowtransparency='true' class='lightwidget-widget' style='width: 100%; border: 0; overflow: hidden;'></iframe>");*/
-				}
-			}
 		},
 
 		app.scroll = {
